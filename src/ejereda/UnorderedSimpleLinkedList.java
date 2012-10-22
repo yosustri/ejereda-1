79142b8101afa07ec2e4180c35d43d01b2f2213f
yosustri <yosustri@aol.com>
2012-10-22 23:10:07 +0300

    Update src/ejereda/UnorderedSimpleLinkedList.java

--- src/ejereda/UnorderedSimpleLinkedList.java
+++ src/ejereda/UnorderedSimpleLinkedList.java
@@ -1,22 +1,57 @@
-package ejereda;
+package listasSimples;
 
 public class UnorderedSimpleLinkedList<T> extends SimpleLinkedList<T> implements UnorderedListADT<T> {
 	
 	public void addToFront(T elem) {
 	// a�ade un elemento al comienzo
-		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-
+		Node<T> result=null;
+		result.data=elem;
+		result.next=first;
+		first=result;
+		count++;
+		//CosteO(1);
 	}
 
 	public void addToRear(T elem) {
 	// a�ade un elemento al final 
-		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-
+		Node<T> result=null;
+		result.data=elem;
+		result.next=last.next;
+		last.next=result;
+		count++;
+		//Coste(1);
 	}
 	
 	public void addAfter(T elem, T target) {
+		//pre:Como minimo hay un elemento en la lista, target.
 	// A�ade elem detr�s de otro elemento concreto, target,  que ya se encuentra en la lista
-		// �COMPLETAR OPCIONAL!
+		if(count==1)
+			addToFront(elem);
+		else
+		{
+		Node<T> current=first;
+		boolean enc=false;
+		Node<T> nuevo=null;
+		Node<T> ant=null;
+		while(current != null&&!enc)
+		{
+			if(current.data.equals(target))
+				enc=true;
+			else
+				{
+				ant=current;
+				current=current.next;
+				}
+		}
+		if(enc)
+		{
+			nuevo.data=elem;
+			nuevo.next=current;
+			ant.next=nuevo;
+			count++;			
+		}
+		}
+		//Coste(n);
 	}
 
 }
