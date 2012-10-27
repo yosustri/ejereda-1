b46b6cc1e7c000fa4a559bc4056eecf0826b8867
yosustri <yosustri@aol.com>
2012-10-27 17:47:58 +0300

    Update src/ejereda/OrderedSimpleLinkedList.java

--- src/ejereda/OrderedSimpleLinkedList.java
+++ src/ejereda/OrderedSimpleLinkedList.java
@@ -1,11 +1,31 @@
-package ejereda;
+package listasSimples;
 
 public class OrderedSimpleLinkedList<T> extends SimpleLinkedList<T> implements OrderedListADT<T> {
 	
 	public void add(T elem){
-		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-
+		Node<T> current=first;
+		Node<T> ant=null;
+		Node<T> nuevo=null;
+		boolean enc=false;
+		while(current != null &&!enc)
+		{
+			if(current.data==elem)
+				enc=true;
+			else
+			{
+				ant=current;
+				current=current.next;
+			}
+		}
+		if(enc)
+		{
+			nuevo.data=elem;
+			nuevo=current;
+			ant.next=nuevo;
+		}
+		//FAlta cuando lista vacia y con un solo elemeto
 	}
 
 
 }
+
